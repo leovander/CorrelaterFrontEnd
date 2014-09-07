136fd056690b98f3976e9899276d04fa7c0eb4cc
Israel Torres <itorres1490@gmail.com>
2014-09-07 02:13:15 -0700

    Removing Android folder in /platforms

--- correlater/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/leovander/Desktop/CECS 491 Project/correlater/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/leovander/Desktop/CECS 491 Project/correlater/platforms/android/CordovaLib/AndroidManifest.xml \
--- correlater/platforms/android/ant-build/Correlater-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/leovander/Desktop/CECS 491 Project/correlater/platforms/android/ant-build/Correlater-debug-unaligned.apk : \
-/Users/leovander/Desktop/CECS 491 Project/correlater/platforms/android/ant-build/Correlater.ap_ \
-/Users/leovander/Desktop/CECS 491 Project/correlater/platforms/android/ant-build/classes.dex \
-/Users/leovander/Desktop/CECS 491 Project/correlater/platforms/android/CordovaLib/ant-build/classes.jar \

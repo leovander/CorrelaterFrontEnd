405f45f47955c567d3ee97c74ffbff2756b67157
Israel Torres <itorres1490@gmail.com>
2014-09-06 21:18:58 -0700

    Added plugin to check device type.

--- correlater/platforms/android/ant-build/classes.dex.d
+++ correlater/platforms/android/ant-build/classes.dex.d
@@ -6,4 +6,5 @@
 /Users/leovander/Desktop/CECS 491 Project/correlater/platforms/android/ant-build/classes/com/example/correlater/R$string.class \
 /Users/leovander/Desktop/CECS 491 Project/correlater/platforms/android/ant-build/classes/com/example/correlater/R$xml.class \
 /Users/leovander/Desktop/CECS 491 Project/correlater/platforms/android/ant-build/classes/com/example/correlater/R.class \
+/Users/leovander/Desktop/CECS 491 Project/correlater/platforms/android/ant-build/classes/org/apache/cordova/device/Device.class \
 /Users/leovander/Desktop/CECS 491 Project/correlater/platforms/android/ant-build/dexedLibs/classes-f59cf0713d74f3587c5ef8ca59abc8fc.jar \

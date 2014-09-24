5764218f8bfd8eb4fb8736deea91558eb2a313fd
Israel Torres <itorres1490@gmail.com>
2014-09-24 15:45:56 -0700

    Cleaned up file headers

--- correlater/www/js/main.js
+++ correlater/www/js/main.js
@@ -1,12 +1,22 @@
 $(document).on('deviceready', function(){
 	var $menu = $('#menu');
 	var $friends = $('#friends');
-	
+
 	$menu.on('click', function(){
 
 	});
 	$friends.on('click', function(){
     window.location='invite.html';
+	});
+
+	$('#logout').click(function() {
+		$.ajax({
+			url: "http://e-wit.co.uk/correlater/user/logout",
+			dataType: 'json',
+			data: $(this).serialize()}
+		).done(function(data){
+				window.location='start.html';
+		});
 	});
 });
 
--- correlater/www/js/start.js
+++ correlater/www/js/start.js
@@ -2,9 +2,10 @@ $(document).on('deviceready', function() {
 	$.ajax({
 			url: "http://e-wit.co.uk/correlater/user/getMyInfo",
       dataType: 'json'}).done(function(data){
-			$('#feedBack').html(data.message+': '+data.user.first_name);
-      console.log(data.user);
-      window.location='main.html';
+			$('#feedBack').html(data.message+': '+data.user.email);
+      if(data.message == "Logged In") {
+					window.location='main.html';
+			}
 		});
   var $loginButton = $('#addAccButton');
   var $signIn = $('#signIn');

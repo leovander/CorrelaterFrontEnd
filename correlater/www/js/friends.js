8308f31d78c2873b69a14423f988a1d2040cd9ce
Israel Torres <itorres1490@gmail.com>
2014-09-25 12:31:24 -0700

    Added the Friends page with two subgroups. Has Requests and Friends.

--- correlater/www/js/friends.js
+++ correlater/www/js/friends.js
@@ -3,13 +3,16 @@ var confirmed;
 
 $(document).on('deviceready', function(){
 	var requests;
+	var friends;
 	getRequests();
+	getFriends();
 
 	confirmed = parseInt($('#friendDivider span').html(), 10);
 
 	$('ul').on('click', 'li', function() {
-		if($(this).attr('class') == "pendingRequest") {
+		if($(this).hasClass("pendingRequest")) {
 			acceptFriend($(this).attr('id'));
+			$('#friendsList').listview('refresh');
 		}
 	});
 });
@@ -22,22 +25,26 @@ function acceptFriend(id) {
 		if(data.message == 'Friend Accepted') {
 			$('#' + id).removeClass('pendingRequest');
 			$('#' + id).fadeOut('slow', function() {
-					$('#friendDivider').after($('#' + id));
+					$('#friendsList').append($('#' + id));
 					$('#' + id + " a").addClass('received');
 					pending--;
 					confirmed++;
 					$('#requestDivider span').html(pending);
 					$('#friendDivider span').html(confirmed);
+					$('#' + id + " a").removeClass('ui-icon-plus ui-icon-carat-r ui-btn-icon-right');
+
 					$('#' + id).fadeIn('slow', function() {
 						setTimeout(function() {
 							$('#' + id + " a").removeClass('received');
 						}, 2000);
 					});
-			});
+	 		});
 		} else {
 			$('ul a').addClass('failed');
 		}
 	});
+
+	$('#requests').listview('refresh');
 }
 
 function getRequests() {
@@ -47,14 +54,35 @@ function getRequests() {
 	).done(function(data){
 		requests = $.makeArray(data.friends);
 		requests.forEach(fillRequests);
-		$('#requestDivider span').html(data.count);
-		$('ul').listview('refresh');
-		pending = parseInt($('#requestDivider span').html(), 10);
+		pending = data.count;
+		$('#requestDivider span').html(pending);
+		$('#requests').listview('refresh');
 	});
 }
 
 function fillRequests(element, index, array) {
-	$('#friendDivider').before('<li id="' + element.id + '" class="pendingRequest">' +
-	'<a href="#">' + element.first_name + ' ' + element.last_name.substring(0,1).toUpperCase() + '.</a>' +
+	$('#requests').append('<li id="' + element.id + '" class="pendingRequest">' +
+	'<a href="#">' + element.first_name + ' ' + element.last_name.substring(0,1).toUpperCase() + '</a>' +
 														 '</li>');
+	$('#' + element.id + ' a').removeClass('ui-icon-carat-r').addClass('ui-icon-plus');
+}
+
+function getFriends() {
+	$.ajax({
+		url: "http://e-wit.co.uk/correlater/user/getFriends",
+		dataType: 'json'}
+	).done(function(data){
+		friends = $.makeArray(data.friends);
+		friends.forEach(fillFriends);
+		confirmed = data.count;
+		$('#friendDivider span').html(confirmed);
+		$('#friendsList').listview('refresh');
+		$('#friendsList li a').removeClass('ui-icon-carat-r ui-btn-icon-right');
+	});
+}
+
+function fillFriends(element, index, array) {
+	$('#friendsList').append('<li id="' + element.id + '">' +
+	'<a href="#">' + element.first_name + ' ' + element.last_name.substring(0,1).toUpperCase() + '</a>' +
+														'</li>');
 }

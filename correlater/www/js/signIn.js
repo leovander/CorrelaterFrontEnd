b1448946fb24346a7543e15c06f3442a5780129c
Israel Torres <itorres1490@gmail.com>
2014-09-25 16:59:50 -0700

    Google Login

--- correlater/www/js/signIn.js
+++ correlater/www/js/signIn.js
@@ -14,4 +14,21 @@ $(document).on('deviceready', function(){
 		});
 		return false;
 	});
+
+	$('#googleLogin').submit(function (event){
+		$.ajax({
+			type: "POST",
+			url: "http://e-wit.co.uk/correlater/user/googleLogin",
+			dataType: 'json',
+			data: $(this).serialize()}
+		).done(function(data){
+			if (data.message == 'Logged In'){
+				$('#feedBack').html(data.message);
+					window.location='main.html';
+			} else {
+				$('#feedBack').html(data.message);
+			}
+		});
+		return false;
+	});
 });

56f4fa06fd57a3e2c02f883ac958554f97894d3d
Israel Torres <itorres1490@gmail.com>
2014-10-18 14:52:55 -0700

    - Changed most naming to Corral. - Added error labeling for form validation. - Form validation for account creation. - Add jQuery Validator - Removed verifyNotify js (Sorry nick)

--- correlater/www/js/createNative.js
+++ correlater/www/js/createNative.js
@@ -1,20 +1,46 @@
 $(function() {
 	document.addEventListener("deviceready", onDeviceReady, false);
 	
-	$('#create').submit(function (event){
-		event.preventDefault();
-		$.ajax({
-			type: "POST",
-			url: "http://e-wit.co.uk/correlater/user/createAccount",
-			dataType: 'json',
-			data: $(this).serialize()}
-		).done(function(data){
-			if (data.message =='Account Created'){
-				window.location='main.html';
+	$("form").validate({
+		rules: {
+			first_name: {
+				required: true	
+			},
+			last_name: {
+				required: true	
+			},
+			last_name: {
+				required: true	
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			password: {
+				required: true
+			},
+			verify_password: {
+		    	required: true,
+		    	equalTo: "#password"
+		    }
+		},
+		errorPlacement: function( error, element ) {
+			error.insertAfter( element.parent() );
+		},
+		submitHandler: function(form) {		
+			$.ajax({
+				type: "POST",
+				url: "http://e-wit.co.uk/correlater/user/createAccount",
+				dataType: 'json',
+				data: $(form).serialize()
+			}).done(function(data){
+				if (data.message =='Account Created'){
+					window.location='main.html';
+					$('#feedBack').html(data.message);
+				}
 				$('#feedBack').html(data.message);
-			}
-			$('#feedBack').html(data.message);
-		});
+			});
+		}
 	});
 });
 

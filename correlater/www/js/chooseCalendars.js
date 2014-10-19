f2c9b485f257b335d9493144b1a2bf3c7a7f2aa9
ny2014 <ungngai@yahoo.com>
2014-10-18 18:58:50 -0700

    - Choose Calendar debug

--- correlater/www/js/chooseCalendars.js
+++ correlater/www/js/chooseCalendars.js
@@ -1,21 +1,30 @@
-$(document).on('deviceready', function(){
-
+$(function() {
+	getCalendars();
 });
 
-
-function getCalendar {
-	$.ajax({
-		type: "POST",
-		url: "http://e-wit.co.uk/correlater/google/getCalendar",
-		dataType: 'json'
-		}
+function getCalendars() {
+	$.getJSON("http://e-wit.co.uk/correlater/google/getCalendars"
 	).done(function(data){
-		console.log(data.calendars); 
-		// if (data.message == 'Logged In'){
-		// 	$('#feedBack').html(data.message);
-		// 		window.location='main.html';
-		// }
-		// $('#feedBack').html(data.message);
+		$.each(data.calendars, function (i, calendar) {
+			$('#chooseCal').append('<label><input type="checkbox" name="'+calendar.id+'">'+calendar.name+'</label>');
+		});
 	});
-	return false;
+}
+
+$('#chooseCal').submit(function (event){
+	event.preventDefault();
+	// console.log($(this).serialize());
+	// $.ajax({
+	// 	type: "POST",
+	// 	url: "http://e-wit.co.uk/correlater/google/confirmCalendars",
+	// 	dataType: 'json',
+	// 	data: $(this).serialize()
+	// 	}).done(function(data){
+	// 	console.log(data);
+	// });
+
+	$('#chooseCal').on("submit", function(event) {
+		event.preventDefault();
+		console.log($(this).serialize() );
+	});
 });
